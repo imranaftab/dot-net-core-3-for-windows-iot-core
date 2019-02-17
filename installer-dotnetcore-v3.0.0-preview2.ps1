65654013581239259fb93f7cc6d7e8c378ab7153
IAftab <iaftab@ssidecisions.com>
2019-02-18 00:03:50 +0500

    fixed constant for .net core runtime file name

--- installer-dotnetcore-v3.0.0-preview2.ps1
+++ installer-dotnetcore-v3.0.0-preview2.ps1
@@ -1,7 +1,7 @@
 ﻿$dotnetSDK = "dotnet-sdk-3.0.100-preview-010184-win-arm.zip"
 $dotnetSDKURL = "https://download.visualstudio.microsoft.com/download/pr/ad976dfc-09d8-429b-9c49-48626898b1bf/680250d6940b02dd7a392321afec9aa6/dotnet-sdk-3.0.100-preview-010184-win-arm.zip"
 
-$dotnetRuntime = "dotnet-sdk-3.0.100-preview-010184-win-arm.zip"
+$dotnetRuntime = "dotnet-runtime-3.0.0-preview-27324-5-win-arm.zip"
 $dotnetRuntimeURL = "https://download.visualstudio.microsoft.com/download/pr/6004c6c3-015e-4900-bb45-b6ad79b05238/74fe4419b74a40895fc88eb27fa413b3/dotnet-runtime-3.0.0-preview-27324-5-win-arm.zip"
 $dotnetDefaultDirectory = "C:\Program Files\dotnet"
 
@@ -21,7 +21,7 @@ if( -Not (Test-Path -Path $dotnetDefaultDirectory)) {
 
 # Install dotnet core sdk
 if(CheckIfFileExists($dotnetSDK)){
-    Write-Host "Intalling .NET Core SDK......"
+    Write-Host "Intalling .NET Core SDK...."
     Expand-Archive $dotnetSDK -DestinationPath $dotnetDefaultDirectory -Force
 } else{
     Write-Warning "Could not find .NET Core SDK binaries zip file $($dotnetSDK) in $($currentDirectory) directory. Trying to Download .NET Core SDK from $($dotnetSDKURL)"
@@ -33,7 +33,7 @@ if(CheckIfFileExists($dotnetSDK)){
 
 # Install dotnet core runtime
 if(CheckIfFileExists($dotnetRuntime)){
-    Write-Host "Intalling .Net Core Runtime......"
+    Write-Host "Intalling .Net Core Runtime...."
     Expand-Archive $dotnetRuntime -DestinationPath $dotnetDefaultDirectory -Force
 } else {
     Write-Warning "Could not find .NET Core Runtime binaries zip file $($dotnetRuntime) in $($currentDirectory) directory. Trying to Download .NET Core Runtime from $($dotnetRuntimeURL)"
