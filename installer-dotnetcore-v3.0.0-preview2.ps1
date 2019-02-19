e55fd868e92fb0be137b0578c796436193816510
IMRAN AFTAB RANA <imranaftab@users.noreply.github.com>
2019-02-19 20:32:59 +0500

    Update installer-dotnetcore-v3.0.0-preview2.ps1
    .NET Core Runtime comes with SDK so not required to installed explicitly

--- installer-dotnetcore-v3.0.0-preview2.ps1
+++ installer-dotnetcore-v3.0.0-preview2.ps1
@@ -1,10 +1,6 @@
 ﻿$dotnetSDK = "dotnet-sdk-3.0.100-preview-010184-win-arm.zip"
 $dotnetSDKURL = "https://download.visualstudio.microsoft.com/download/pr/ad976dfc-09d8-429b-9c49-48626898b1bf/680250d6940b02dd7a392321afec9aa6/dotnet-sdk-3.0.100-preview-010184-win-arm.zip"
-
-$dotnetRuntime = "dotnet-runtime-3.0.0-preview-27324-5-win-arm.zip"
-$dotnetRuntimeURL = "https://download.visualstudio.microsoft.com/download/pr/6004c6c3-015e-4900-bb45-b6ad79b05238/74fe4419b74a40895fc88eb27fa413b3/dotnet-runtime-3.0.0-preview-27324-5-win-arm.zip"
 $dotnetDefaultDirectory = "C:\Program Files\dotnet"
-
 $currentDirectory = $PSScriptRoot;
 
 function CheckIfFileExists([string] $filename){
@@ -31,21 +27,9 @@ if(CheckIfFileExists($dotnetSDK)){
     Expand-Archive $saveSDKPath -DestinationPath $dotnetDefaultDirectory -Force
 } 
 
-# Install dotnet core runtime
-if(CheckIfFileExists($dotnetRuntime)){
-    Write-Host "Intalling .Net Core Runtime...."
-    Expand-Archive $dotnetRuntime -DestinationPath $dotnetDefaultDirectory -Force
-} else {
-    Write-Warning "Could not find .NET Core Runtime binaries zip file $($dotnetRuntime) in $($currentDirectory) directory. Trying to Download .NET Core Runtime from $($dotnetRuntimeURL)"
-    $saveRuntimePath = [System.IO.Path]::Combine($currentDirectory, $dotnetRuntime);
-    Invoke-WebRequest $dotnetRuntimeURL -OutFile $saveRuntimePath
-    Write-Host "Download completed! Installing .NET Core Runtime ..."
-    Expand-Archive $saveRuntimePath -DestinationPath $dotnetDefaultDirectory -Force
-} 
-
 # Add dotnet to system envirnment path if not already in Environment Path
 If ( -NOT $Env:Path.Contains($dotnetDefaultDirectory )) {
-    $Env:Path = $Env:Path+";"+$dotnetDefaultDirectory
+    $Env:Path +=";$($dotnetDefaultDirectory)"
 }
 
 Write-Host "Checking .Net Core version..."
